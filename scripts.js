7d26a0592e13b1e92707ef9103eab8a0156f39cb
Terry Jachimiak II <terry.jachimiak@gmail.com>
2025-07-11 15:43:07 -0400

    EDIT Javascript file

--- scripts.js
+++ scripts.js
@@ -1,4 +1,8 @@
 
+// Populate the Grid
+
+let color = "black";
+let fade = false;
 
 function makeGrid(size) {
     let screen = document.querySelector(".sketch-screen");
@@ -11,12 +15,48 @@ function makeGrid(size) {
             row.style.borderBottom = "1px solid rgb(165, 165, 165)";
             row.style.borderRight = "1px solid rgb(165, 165, 165)";
             column.appendChild(row);
+            if (fade === false) {
+                row.addEventListener('mouseover', changeBlackOrColor);
+            } else if (fade === true) {
+                row.addEventListener('mouseover', fadeToDark);
+            }
         }
         screen.appendChild(column);
     }
 }
 
-
-
 makeGrid(16);
+
+// Switch between black and color versions.
+function changeBlackOrColor(){
+    if (color === "black") {
+        this.style.background = "black";
+    } else if (color === "rainbow") {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    }
+}
+
+// Accept choice from web UI.
+function chooseBlackOrColor(choice) {
+    color = choice;
+}
+
+// Choice to fade to dark.
+/* function fadeToDark() {
+    if (color === "black" && fade === true) {
+        let fadeUpColor = 0.05;
+        this.style.backgroundColor = "rgba(0, 0, 0, " + fadeUpColor + ")";
+    }
+} */
+
+// Clear the board to white.
+function resetBoard() {
+    let container = document.querySelector(".sketch-container");
+    let squares = container.querySelector("div");
+    squares.forEach((div) => (div.style.backgroundColor = "white"));
+}
+
+
+
+
 
